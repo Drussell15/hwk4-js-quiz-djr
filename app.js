54cca278bcd43823d453e782226ec5aa717e6c10
Drussell15 <62867827+Drussell15@users.noreply.github.com>
2020-06-15 03:54:55 -0700

    create app.js and declare vars
    declared needed variables

---
+++ app.js
@@ -0,0 +1,9 @@
+//declare variables
+var score= 0;
+var questinIndex = 0;
+
+//variables needed or timing to work
+var currentTime= document.querySelector("#currentTime");
+var timer= document.querySelector("#startTime");
+var questionDiv =document.querySelector("#questionDiv");
+var wrapper= document.querySelector("#wrapper");
