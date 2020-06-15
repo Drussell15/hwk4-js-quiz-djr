db23838afc577fa0cddaeabd00d05bf62f7ef27a
Drussell15 <62867827+Drussell15@users.noreply.github.com>
2020-06-15 03:58:24 -0700

    added timings
    created variables for time between q's and penalty time

--- app.js
+++ app.js
@@ -7,3 +7,13 @@ var currentTime= document.querySelector("#currentTime");
 var timer= document.querySelector("#startTime");
 var questionDiv =document.querySelector("#questionDiv");
 var wrapper= document.querySelector("#wrapper");
+
+//if user is given 15 seconds to answer each question
+var secondsLeft= 76;
+//holding interval time
+var holdInverval= 0;
+//hold time penalty
+var penalty= 10;
+//create new element
+var ulCreate= document.createElement("ul");
+
