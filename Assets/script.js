0a646f05b83c7850d76d2abeb26f589ec48aff12
Drussell15 <62867827+Drussell15@users.noreply.github.com>
2020-06-17 22:26:40 -0700

    updated
    declared elements needed

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,13 @@
-js script
+//declare elements
+var start = document.getElementById("startPage");
+var quiz = document.getElementById("quiz");
+var question = document.getElementById("question");
+var choiceA = document.getElementById('A');
+var choiceB = document.getElementById('B');
+var choiceC = document.getElementById('C');
+var choiceD = document.getElementById('D');
+var next = document.getElementById("next");
+var submit = document.getElementById("submit");
+var hsForm = document.getElementById("hs-form");
+var initialsInput = document.querySelector("#initials-text")
+
